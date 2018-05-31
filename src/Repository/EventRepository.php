aaa2180c2a216e321b9a92d252f6f91ea2a0741d
Benoit Galati <benoit.galati@gmail.com>
2018-05-31 14:54:11 +0200

    [CH3647] Add route to count total event partipants in referent areas
    https://app.clubhouse.io/enmarche/story/3647/3e-partie-section-evenements-box-kpis-inscrits-events

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -515,6 +515,28 @@ SQL;
         ;
     }
 
+    public function countParticipantsInReferentManagedArea(Adherent $referent): int
+    {
+        $this->checkReferent($referent);
+
+        $results = $this->createQueryBuilder('event')
+            ->select('event.participantsCount')
+            ->innerJoin('event.referentTags', 'tag')
+            ->where('tag IN (:tags)')
+            ->andWhere('event.committee IS NOT NULL')
+            ->andWhere("event.status = '".Event::STATUS_SCHEDULED."'")
+            ->andWhere('event.organizer != :referent')
+            ->andWhere('event.participantsCount > 0')
+            ->groupBy('event.id')
+            ->setParameter('tags', $referent->getManagedArea()->getTags())
+            ->setParameter('referent', $referent)
+            ->getQuery()
+            ->getArrayResult()
+        ;
+
+        return array_sum(array_column($results, 'participantsCount'));
+    }
+
     public function countCommitteeEventsInReferentManagedArea(Adherent $referent, StatisticsParametersFilter $filter = null): array
     {
         $this->checkReferent($referent);
